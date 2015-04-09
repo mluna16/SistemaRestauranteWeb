f74acf0c115b981b5308374e75274eb919b53b50
marcoslunah <mlunag16@gmail.com>
2015-04-09 09:50:36 -0430

    Cambio de mensaje a espa;ol, para subir archivos

--- resources/views/partials/admin/ModalCreateMenu.blade.php
+++ resources/views/partials/admin/ModalCreateMenu.blade.php
@@ -51,17 +51,17 @@
                     'method' => 'POST',
                     ])
             !!}
-
+            <h6 class="dz-message"><i class="medium mdi-editor-attach-file"></i>Arrastra hasta o has click aqui para agregar images al producto (Maximo 5)</h6>
             {!!Form::close()!!}
         </div>
 
     </div>
     <div class="modal-footer">
-        <div class="container">
+        <!-- <div class="container">
             <div class="progress">
                 <div class="determinate" style="width: 0%"></div>
             </div>
-        </div>
+        </div> -->
         <a href="#"  class="waves-effect waves-green btn-flat modal-action crear_menuSubmit ">Siguiente</a>
         <a href="#"  style="display:none"class="waves-effect waves-green btn-flat modal-action  crear_menuSubmitnone menuPaso1">Siguiente</a>
         <a href="#"  style="display:none"class="waves-effect waves-green btn-flat modal-action  menuPaso2">Finalizar</a>
