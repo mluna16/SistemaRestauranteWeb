b4aa6ae3bbcfbe32876fd2126c08d6d2114d9c39
marcoslunah <mlunag16@gmail.com>
2015-04-14 12:25:57 -0430

    Parallax para el inicio

---
+++ resources/views/layout/parallax.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Restaurante | @yield('title','Restaurante')</title>
+    <!-- CSS  -->
+    <link href="{{asset('css/materialize.css')}}" type="text/css" rel="stylesheet" />
+    <link href="{{asset('css/parallax.css')}}" type="text/css" rel="stylesheet" />
+
+    <style>
+
+
+
+    </style>
+</head>
+<body>
+    @section('contentParallax')
+
+    @show
+<script src="{{asset('js/jQueryMaster.js')}}"></script>
+<script src="{{asset('js/materialize.js')}}"></script>
+<script src="{{asset('js/parallax.js')}}"></script>
+</body>
--- resources/views/welcomeP.blade.php
+++ resources/views/welcomeP.blade.php
@@ -1,80 +1,128 @@
-@extends('layout.init')
+@extends('layout.parallax')
 
-@section('title')
-    Inicio
-@stop
-@section('alternalCSS')
-    <link href="{{asset('css/paralax.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <style>
-        .parallax-container {
-            height: 500px;
-        }
-    </style>
+@section('contentParallax')
+    <nav class="white" role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Logo</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="{{url('auth/register')}}">Crear cuenta</a></li>
+                <li><a href="{{url('auth/login')}}">Iniciar sesion</a></li>
+            </ul>
 
-@endsection
-@section('content')
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="{{url('auth/register')}}">Crear cuenta</a></li>
+                <li><a href="{{url('auth/login')}}">Iniciar sesion</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+        </div>
+    </nav>
 
-    @include('partials.principal.headerPrincipal')
-    <style type="text/css">
-        .parallaxParent {
-            height: 100vh;
-            overflow: hidden;
-        }
-        .parallaxParent > * {
-            height: 200%;
-            position: relative;
-            top: -100%;
-        }
-    </style>
-    <div class="spacer s0"></div>
-    <div id="parallax1" class="parallaxParent">
-        <div style="background-image: url('http://janpaepke.github.io/ScrollMagic/img/example_parallax_bg1.png');"></div>
+    <div id="index-banner" class="parallax-container">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <br><br>
+                <h1 class="header center orange-text text-darken-4">No tenemos nombre aun</h1>
+                <div class="row center">
+                    <h5 class="header col s12 brown-text text-darken-3 light">Pero este sistema web y movíl para <span class="red-text text-darken-4">Restaurante</span> lo tendra! </h5>
+                </div>
+
+                <br><br>
+
+            </div>
+        </div>
+        <div class="parallax" ><img  src="{{asset('images/restaurante3.jpg')}}" class="img1" alt="Unsplashed background img 2"></div>
     </div>
-    <div class="spacer s1">
-        <div class="box2 blue">
-            <p>Content 1</p>
-            <a href="#" class="viewsource">view source</a>
+
+
+    <div class="container">
+        <div class="section">
+
+            <!--   Icon Section   -->
+            <div class="row">
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center green-text  text-darken-4"><i class="mdi-action-face-unlock"></i></h2>
+                        <h5 class="center">Desarrollado para Personas</h5>
+
+                        <p class="light">El sistema esta efocado para que cualquier persona pueda usarla sin complejidad.</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center green-text  text-darken-4"><i class="mdi-social-group"></i></h2>
+                        <h5 class="center">User Experience Focused</h5>
+
+                        <p class="light">Son dos sistemas en uno. Por un lado tienes el sistema web para gestinar el restaurante y manejar la caja. Y otro una aplicacion Android para manejar las Mesas, los pedidos y la cocina.</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m4">
+                    <div class="icon-block">
+                        <h2 class="center green-text  text-darken-4"><i class="mdi-hardware-cast-connected"></i></h2>
+                        <h5 class="center">Chromecast</h5>
+
+                        <p class="light">Puedes tener toda la aplicacion Android enlazada en tu dispositivo Chromecast.</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
-    <div class="spacer s0"></div>
-    <div id="parallax2" class="parallaxParent">
-        <div style="background-image: url('http://janpaepke.github.io/ScrollMagic/img/example_parallax_bg1.png');"></div>
+
+
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+                <div class="row center">
+                    <h4 class="header col s12 thin">Maneja las ordenes y los pedidos de tu <span class="light-green-text text-lighten-3">Restaurante</span> de forma ordenada y eficiente</h4>
+                </div>
+
+        </div>
+        <div class="parallax"><img src="{{asset('images/restaurante2.jpg')}}" alt="Unsplashed background img 2"></div>
     </div>
-    <div class="spacer s1">
-        <div class="box2 blue">
-            <p>Content 2</p>
-            <a href="#" class="viewsource">view source</a>
+
+    <div class="container">
+        <div class="section">
+
+            <div class="row">
+                <div class="col s12 center">
+                    <h3><i class="green-text  text-darken-4 mdi-hardware-keyboard-alt"></i></h3>
+                    <h4>¿ Por qué somos útiles para tí ?</h4>
+                    <p class="left-align light">El reemplazo de sistemas anticuados o desactualizados como el de pedidos en un restaurante por un sistema móvil que se encargue de la comunicación de los mesoneros con la cocina, contribuirá a que estos procesos sean más eficientes y atractivos, además que se unen a la idea de un mundo cada vez más evolucionado que aprovecha las nuevas tecnologías para hacer cómodos y funcionales hasta los procesos cotidianos. En este sentido, el propósito de la investigación es precisamente construir una aplicación informática que combine las prestaciones de las aplicaciones móviles con el uso de las web y dispositivos streaming. De esta manera, atender de mejor forma las solicitudes de servicio y despacho en un restaurante, tal como el caso de estudio</p>
+                </div>
+            </div>
+
         </div>
     </div>
-    <div class="spacer s0"></div>
-    <div id="parallax3" class="parallaxParent">
-        <div style="background-image: url('http://janpaepke.github.io/ScrollMagic/img/example_parallax_bg1.png');"></div>
+
+
+    <div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+                <div class="row center">
+                    <h4 class="header col s12 thin">Nos adaptamos a su <span class="red-text text-darken-4">Restaurante</span>, no importa si tiene 2 mesas o <span class="red-text text-lighten-1">1.000</span></h4>
+                </div>
+        </div>
+        <div class="parallax"><img src="{{asset('images/restaurante4.jpg')}}" alt="Unsplashed background img 3"></div>
     </div>
-    <div class="spacer s2"></div>
 
-@endsection
+    <footer class="page-footer  green darken-1">
+        <div class="container">
+            <div class="row">
+                <div class="col  s12">
+                    <h5 class="white-text">No tenemos nombre aun !</h5>
+                    <p class="grey-text text-lighten-4">Este Sistema está siendo desarrollado como proyecto tesis para la obtención de un título Universitario de Licenciado en Informática de la Universidad de Oriente Núcleo Nueva Esparta.</p>
 
-@section('alternalJS')
-    <script src="http://janpaepke.github.io/ScrollMagic/js/lib/greensock/TweenMax.min.js"></script>
-    <script src="http://janpaepke.github.io/ScrollMagic/scrollmagic/uncompressed/ScrollMagic.js"></script>
-    <script src="http://janpaepke.github.io/ScrollMagic/scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
-    <script src="http://janpaepke.github.io/ScrollMagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js"></script>
 
-    <script>
-        // init controller
-        var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+                </div>
 
-        // build scenes
-        new ScrollMagic.Scene({triggerElement: "#parallax1"})
-                .setTween("#parallax1 > div", {y: "80%", ease: Linear.easeNone})
-                .addTo(controller);
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Made for <a class="text-white" href="https://www.facebook.com/pedro.moyahernandez">Pedro Moya</a>
+                & <a class="text-white" href="http://marcoslunah.herokuapp.com">Marcos Luna H</a>
 
-        new ScrollMagic.Scene({triggerElement: "#parallax2"})
-                .setTween("#parallax2 > div", {y: "80%", ease: Linear.easeNone})
-                .addTo(controller);
-
-        new ScrollMagic.Scene({triggerElement: "#parallax3"})
-                .setTween("#parallax3 > div", {y: "80%", ease: Linear.easeNone})
-                .addTo(controller);
-    </script>
+            </div>
+        </div>
+    </footer>
 @endsection
