10b3d4331d3cc4289bbafb7286bab739d39c9d0f
marcoslunah <mlunag16@gmail.com>
2015-04-17 08:58:51 -0430

    Cambio como test de APIREST para el el modelo producto y el controllador producto

--- app/Http/Controllers/productsController.php
+++ app/Http/Controllers/productsController.php
@@ -1,6 +1,8 @@
 <?php namespace SistemaRestauranteWeb\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use Psy\Exception\Exception;
 use SistemaRestauranteWeb\Http\Requests;
 use SistemaRestauranteWeb\Http\Controllers\Controller;
 
@@ -12,29 +14,29 @@ use SistemaRestauranteWeb\User;
 /**
  * @property mixed CreatedBy
  */
-class productsController extends Controller {
+class productsController extends Controller
+{
 
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -42,73 +44,103 @@ class productsController extends Controller {
      * @param CreateProductsRequest $request
      * @return Response
      */
-	public function store(CreateProductsRequest $request)
-	{
+    public function store(CreateProductsRequest $request)
+    {
 
         $Product = Product::create($request->all());
         $Product->setCreatedByAttribute($Product->created_by);
         $Product->setLocalForAttribute(1);
-       if($Product->save()){
+        if ($Product->save()) {
 
-           if($request->ajax()) return array('last_id' => $Product->id );
+            if ($request->ajax()) return array('last_id' => $Product->id);
 
         };
-	    return $Product;
+        return $Product;
 
     }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		$product = new Product();
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $product = new Product();
         return $product->getProductsForId($id);
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-    public  function getProductsForAttrAndID($attr , $value){
-        $Product = new Product();
-        return $Product->getProductsForAttrAndID($attr , $value);
     }
 
-    public function getProductForUser($value){
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getProductsForAttrAndID($attr, $value)
+    {
         $Product = new Product();
-        return $Product->getProductsForCreatedBy($value);
+        return $Product->getProductsForAttrAndID($attr, $value);
+    }
+
+    public function getProductForUser($value)
+    {
+
+        $Product = new Product();
+
+        try {
+            $response = [
+                'Products' => []
+            ];
+
+            $statusCode = 200;
+            $products = $Product->getProductsForCreatedBy($value);
+
+            foreach ($products as $product) {
+                $response['Products'][] = [
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'cost' => $product->cost,
+                    'description' => $product->description,
+                    'limit' => $product->limit,
+                ];
+            }
+        }catch (Exception $e) {
+            $response = [
+                "error" => "No hay productos para este usuario"
+            ];
+            $statusCode = 404;
+
+        } finally {
+            return Response::json($response, $statusCode);
+        }
     }
 
     public function getProductForLocal($value){
