41a78239d2703a4df75c04b8f7261b0a2a3f9a10
marcoslunah <mlunag16@gmail.com>
2015-04-30 16:09:19 -0430

    Configurando OrderController para la Api

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,105 @@
+<?php namespace SistemaRestauranteWeb\Http\Controllers\Api;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+use SistemaRestauranteWeb\Http\Requests;
+use SistemaRestauranteWeb\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use SistemaRestauranteWeb\Local;
+use SistemaRestauranteWeb\Order;
+use SistemaRestauranteWeb\Table;
+
+
+class OrderController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+
+
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Request $request)
+	{
+        $local = new Local();
+
+        $this->validate($request, [
+            'idTable' => 'required', 'idProduct' => 'required',
+        ]);
+
+        $orderAttr = ['id_product' => $request->idProduct, 'created_by' => Auth::user()->id,'state' => 'espera'];
+        $order = Order::create($orderAttr);
+
+        if($order->save()){
+            $tableAttr = ['number_table' => $request->idTable, 'id_order' => $order->id,'state' => 'ocupado','id_local' => $local->getLocalIdAttribute()];
+            $table = Table::create($tableAttr);
+            if($table->save()) return Response::json(array('success' => true),200);
+            else return Response::json(array('success' => false),500);
+        }
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
