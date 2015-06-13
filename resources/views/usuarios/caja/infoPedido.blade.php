b2b848a39afa684c94c909c52853cf3422896cfe
marcoslunah <mlunag16@gmail.com>
2015-06-13 15:10:23 -0430

    Solucionado problema para actulizar el estado de las mesas

--- resources/views/usuarios/caja/infoPedido.blade.php
+++ resources/views/usuarios/caja/infoPedido.blade.php
@@ -1,64 +1,26 @@
-@extends('usuarios.caja.infoPedido')
-
-@section('InfoPedido')
-    @if(isset($data['data']['CostTable']))
-        <div id="infoPedido">
-            <div class="col s12 m12 l6">
-                <div class="row">
-                    <div class="col s12 m12 l12">
-                        <div class="card large">
-                            <div class="card-content " >
-                                <h4>Mesa Numero {{$data['data']['NumberTable']}}</h4>
-                                <table class="table-responsive">
-                                    <thead>
-                                    <tr>
-                                        <th data-field="producto">Producto</th>
-                                        <th data-field="precio">Precio</th>
-                                        <th data-field="estado">Estado</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($data['data']['Pedidos'] as $pedidos)
-                                        <tr data-id="{{$pedidos['ProductId']}}">
-                                            <td> {{$pedidos['ProductName']}}</td>
-                                            <td> {{$pedidos['ProductCost']}}</td>
-                                            <td> {{$pedidos['OrderState']}}</td>
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
+@extends('usuarios.caja.caja')
+@section('tablesPanel')
+    <div class="col s12 m12 l6">
+        <div class="row">
+            @for($i=1; $i <= $data['TotalMesas'];$i++)
+                @if($data['Mesas'][$i-1]['State'] == 'ocupado')
+                    <div class="col s6 m4 l3" style="cursor: pointer">
+                        <a class="mostrar_mesa tooltipped" data-id="{{$i}}" data-position="right" data-delay="50" data-tooltip="{{$data['Mesas'][$i-1]['State']}}">
+                            <div class="card-panel red lighten-3 ">
+                                <h5 class="black-text center-align ">{{$i}} </h5>
                             </div>
-                            <div class="card-action light-green lighten-1">
-                                <div class="row">
-                                    <div class="col s8 m8 l8">
-                                        <h5 class="left-align">Total Bs : {{$data['data']['CostTable']}}} </h5>
-                                    </div>
-                                    <div class="col s4 m4 l4">
-                                        <a class="btn  yellow lighten-4">Facturar</a>
-                                    </div>
-                                </div>
+                        </a>
+                    </div>
+                @else
+                    <div class="col s6 m4 l3">
+                        <a class="tooltipped" data-position="right" data-delay="50" data-tooltip="{{$data['Mesas'][$i-1]['State']}}">
+                            <div class="card-panel green lighten-3 ">
+                                <h5 class="black-text center-align ">{{$i}} </h5>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                </div>
-            </div>
+                @endif
+            @endfor
         </div>
-    @else
-        <div class="col s12 m12 l6">
-            <div class="row">
-                <div class="col s12 m12 l12">
-                    <div class="card large">
-                        <div class="card-content " >
-
-                            <p class="">
-                                Seleccione una mesa ocupada
-                            </p>
-                        </div>
-                        <div class="card-action light-green lighten-1">
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endif
-@endsection
+    </div>
+@stop
